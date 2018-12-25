b40bab3f0dedf8e2b7fdd36f09b80d72dbb996ee
Vishesh <visheshsingh16@gmail.com>
2018-12-25 15:38:50 -0500

    GraphQL + MongoDB

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const bodyParser = require("body-parser");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/events");
 
 const app = express();
 app.use(bodyParser.json());
@@ -42,22 +45,57 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        // const event = {
+        //   _id: Math.random().toString(),
+        //   title: args.eventInput.title,
+        //   description: args.eventInput.description,
+        //   price: +args.eventInput.price,
+        //   date: args.eventInput.date
+        // };
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date)
+        });
+
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result.id };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     graphiql: true
   })
 );
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+mongoose
+  .connect(
+    `mongodb://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@ds141623.mlab.com:41623/gql-events`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch(err => {
+    console.log(err);
+  });
